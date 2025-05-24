47a88a2cbc6d85226d27cc57bc836d08ace7c044
reaperlord10 <reaperlordsgaming@gmail.com>
2025-05-24 23:37:59 +0530

    pinctrl: qcom: Declare missing 'offset' variable
    Fix build failure caused by the use of 'offset' in a GPIO-related
    conditional without prior declaration. This became an issue after
    merging upstream changes which rely on 'offset' being passed to
    is_gpio_tlmm_dc().
    
    Fixes: merge of upstream changes involving TLMM DC support
    Signed-off-by: reaperlord10 <reaperlordsgaming@gmail.com>

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -1222,6 +1222,7 @@ static int msm_gpio_irq_set_type(struct irq_data *d, unsigned int type)
 	irq_hw_number_t irq = 0;
 	u32 intr_target_mask = GENMASK(2, 0);
 	unsigned long flags;
+	u32 offset = 0;
 	u32 val, oldval;
 
 	if (msm_gpio_needs_dual_edge_parent_workaround(d, type)) {
